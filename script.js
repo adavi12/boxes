68b4c187dcd56327c265bc1c59d9e4e9fa524a2b
Ahkillah Davis <ahkillahdavis@gmail.com>
2017-01-10 20:53:38 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("JS");
+$(document).ready(function(){
+  console.log("JQ");
+  $("#addBox").on("click", function(){
+    console.log("addBox click");
+    $("#container").append('<div class ="box"></div>');
+    $('.box').click(function() {
+          $(this).css('background', 'black');
+      });
+  });
+});
+
+//create array for random color
